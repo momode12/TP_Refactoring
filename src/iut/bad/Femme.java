6c56fabb8cf77f0f84f7ef496a890ec36e3d94c8
momode12 <heritianajulien12@gmail.com>
2026-03-30 08:14:26 +0300

    main dans femme

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -5,4 +5,12 @@ public class Femme extends Humain {
     public Femme(String nom, String prenom, int age) {
         super(nom, prenom, age);
     }
+    
+    public static void main(String[] args) {
+        Homme h = new Homme("HERITIANA", "Julien", 23);
+        Femme f = new Femme("CARL", "Mômô", 20);
+
+        h.ami(f); 
+        f.ami(h); 
+    }
 }
